928d49e3dd4df0655c45e50391095f308a260999
gbs4ever <gbs4ever2@gmail.com>
2021-11-24 12:37:04 -0500

    git output untracked also

--- .githelper.sh
+++ .githelper.sh
@@ -1,4 +1,4 @@
-alias clean="git ls-files -m| xargs -r rubocop"
+alias clean="git ls-files --others -m --exclude-standard| xargs -r rubocop"
 alias glo='git log --graph --pretty=format:"%Cred%h%Creset - %s %Cgreen(%cr)%Creset" --abbrev-commit --date=relative'
 alias gp="g push origin HEAD"
 
