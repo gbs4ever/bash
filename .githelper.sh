c7c3e41962472660f25b96369cd18af0d8099319
gbs4ever <gbs4ever2@gmail.com>
2021-11-24 11:49:25 -0500

    clean and test

--- .githelper.sh
+++ .githelper.sh
@@ -1,3 +1,10 @@
 alias clean="git ls-files -m| xargs -r rubocop"
 alias glo='git log --graph --pretty=format:"%Cred%h%Creset - %s %Cgreen(%cr)%Creset" --abbrev-commit --date=relative'
 alias gp="g push origin HEAD"
+
+
+function cltest(){
+  echo "run rubocop then rspec....."
+clean && rspec
+
+}  
