be756493a1a638cf7a67e06a5ab15284217bfc66
gbs4ever <gbs4ever2@gmail.com>
2021-11-24 21:28:24 -0500

    skip typeing git commit

--- .githelper.sh
+++ .githelper.sh
@@ -1,7 +1,9 @@
 alias clean="git ls-files --others -m --exclude-standard| xargs -r rubocop"
 alias glo='git log --graph --pretty=format:"%Cred%h%Creset - %s %Cgreen(%cr)%Creset" --abbrev-commit --date=relative'
 alias gp="g push origin HEAD"
-
+ function gitcommit(){
+    git commit -m $1
+ }
 
 function cltest(){
   echo "To run rubocop with auto correct type -A or just hit enter " 
