b02d41241b59d03943c1727a90575c192f0bb53d
gbs4ever <gbs4ever2@gmail.com>
2024-12-13 13:34:25 -0500

    fixed readme

--- package.sh
+++ package.sh
@@ -1,22 +1,22 @@
 find(){
   echo installing...
-sudo apt-get install fzf
-sudo apt-get install fd-find
+  sudo apt-get install fzf
+  sudo apt-get install fd-find
 }
 postgres_install() {
   logger "Installing postgres..."
-sudo apt install -q -y postgresql postgresql-contrib libpq-dev
-    logger "Successfully installed postgres client and server"
+  sudo apt install -q -y postgresql postgresql-contrib libpq-dev
+  logger "Successfully installed postgres client and server"
 
 }
 wsl_setup(){
-sudo apt-get update  
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev libpq-dev libgdbm-dev libncurses5-dev automake libtool bison gnupg postgresql postgresql-contrib
+  sudo apt-get update  
+  sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev libpq-dev libgdbm-dev libncurses5-dev automake libtool bison gnupg postgresql postgresql-contrib
 }
 rvm(){
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-sudo apt-get install curl
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+  sudo apt-get install curl
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby
 }
 
 
