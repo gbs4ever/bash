2fdd9e5c7e462c0b4b4223b0aba0117ba9b69be2
gbs4ever <gbs4ever2@gmail.com>
2025-03-27 16:12:41 -0400

    FIX: cleaner option to ssh into heroku

--- .updates.sh
+++ .updates.sh
@@ -12,31 +12,4 @@ upgrade_linux (){
 }
 
 
-### ruby gems update with bundler #####
-function gem_update() {
-  echo "Updating gems >>>"
-  logger "Running bundle update ........"
-  update_output=$(bundle update)
-  if [ $? -eq 0 ]; then
-    echo "Bundle update completed successfully."
-  else
-    echo "Error: Bundle update failed."
-    return 1
-  fi
-  if ! git diff --quiet --exit-code Gemfile.lock; then
-    # Run RuboCop
-    echo "Running RuboCop..."
-    rubocop -A
-    # Commit changes to Gemfile.lock with update output as part of the commit message
-    echo "Committing changes..."
-    git add Gemfile.lock
-    git commit -m "Update gems: $(echo "$update_output" | grep -E '^Bundle complete!' | tail -1)"
-  
-    # Push changes to Git repository
-    echo "Pushing changes to Git repository..."
-    git push
-  else
-    echo "No changes to commit, git exit "
-  fi
-  
-}
+
